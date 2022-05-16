d6eff31d6e09eee51a4b22e3fe4b9e54538a5278
smulCodeMonk <103043966+smulCodeMonk@users.noreply.github.com>
2022-05-16 16:08:27 +0200

    Add files via upload

---
+++ src/components/Spritesheet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styles from './Spritesheet.module.scss';
+
+export default class Spritesheet extends Component {
+    render() {
+        return;
+        <div className={styles.container}>
+            <canvas id="canvas"></canvas>
+        </div>;
+    }
+}
